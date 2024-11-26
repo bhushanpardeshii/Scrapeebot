51b96044de0d70eeee03827ae1a00e9fa4c338b0
Bhushan Pardeshi <bhushanpardeshi549@gmail.com>
2024-11-26 19:51:01 +0530

    eth balance

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-const { Alchemy, Network } = require("alchemy-sdk");
+const { Alchemy, Network, Utils } = require("alchemy-sdk");
 
 const settings = {
     apiKey: "q_pyhzHmpZkFEWipu_M4t4Ea5z-DVXAg",
@@ -14,10 +14,12 @@ const main = async () => {
     alchemy.ws.on("block", (blockNumber) => {
         block = blockNumber;
         console.log("Latest block:", blockNumber);
+        runmain(blockNumber - 1)
     });
 
     // Periodically fetch transaction receipts for the latest block
-    setInterval(async () => {
+    const runmain = async (block) => {
+        console.log('block number in', block)
         if (block !== null) {
             const hexBlock = block.toString(16);
             console.log("Latest block accessed outside in hex:", hexBlock);
@@ -25,19 +27,37 @@ const main = async () => {
             try {
                 // Fetch transaction receipts for the current block
                 const params = { blockNumber: `0x${hexBlock}` };
+                // fetch contract address 
                 const response = await alchemy.core.getTransactionReceipts(params);
-                console.log("Transaction Receipts:", response);
+                response.receipts.forEach(receipt => {
+                    if (receipt.contractAddress) {
+                        console.log('contract address', receipt.contractAddress)
+                    }
+                })
+                // console.log("Transaction Receipts:", response);
             } catch (error) {
                 console.error("Error fetching transaction receipts:", error.message);
             }
+            // check if its erc20 token by checking decimals
+            //0x704387ead0c9e4a8c8fd70472d6cbae6fa23eb4f
+            const usdcContract = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D";
+            var tokenmetadata = await alchemy.core.getTokenMetadata(usdcContract)
+            console.log(tokenmetadata.decimals)
         } else {
             console.log("Waiting for the first block to arrive...");
         }
-    }, 5000);
-    const usdcContract = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D";
-    var tokenmetadata = alchemy.core.getTokenMetadata(usdcContract)
-    console.log(tokenmetadata)
+        if (tokenmetadata.decimals == null || 0) {
+            return
+        } else {
 
+            //to get the eth in the contract        
+            const address = "0xB8c77482e45F1F44dE1745F52C74426C631bDD52";
+            let balance = await alchemy.core.getBalance(address);
+            balance = Utils.formatEther(balance);
+            console.log(`Balance of ${address}: ${balance} ETH`);
+        }
+
+    }
 };
 
 main();
